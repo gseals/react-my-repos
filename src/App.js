d2eeb27e3b8c07f22e6b8e7ed509d4d6b2371571
Gabriel Seals <sealsgj@gmail.com>
2020-06-16 12:31:16 -0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
-
+import List from './components/List';
+import withListLoading from './components/withListLoading';
 function App() {
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  // useEffect(() => {
+  //   setAppState({ loading: true });
+  //   const apiUrl = `https://api.github.com/users/hacktivist123/repos`;
+  //   fetch(apiUrl)
+  //     .then((res) => res.json())
+  //     .then((repos) => {
+  //       setAppState({ loading: false, repos: repos });
+  //     });
+  // }, [setAppState]);
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = 'https://api.github.com/users/hacktivist123/repos';
+    axios.get(apiUrl).then((repos) => {
+      const allRepos = repos.data;
+      setAppState({ loading: false, repos: allRepos });
+    });
+  }, [setAppState]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <h1>My Repositories</h1>
+      </div>
+      <div className='repo-container'>
+        <ListLoading isLoading={appState.loading} repos={appState.repos} />
+      </div>
+      <footer>
+        <div className='footer'>
+          Built{' '}
+          <span role='img' aria-label='love'>
+            💚
+          </span>{' '}
+          with by Shedrack Akintayo
+        </div>
+      </footer>
     </div>
   );
 }
-
 export default App;
